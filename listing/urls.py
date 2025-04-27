2a4265c122346a187f5c52a5a3dcbf1e42667af1
mnhtndev <tuannguyen.02042005@gmail.com>
2025-04-27 09:30:45 +0700

    feat: listings

--- listing/urls.py
+++ listing/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from .views import ListingViewSet, ReviewViewSet
+from .views import ListingViewSet, ReviewViewSet, ListingImageViewSet
 
 router = DefaultRouter()
-# router.register(r'listings', ListingViewSet)
-# router.register(r'reviews', ReviewViewSet)
+router.register(r'listings', ListingViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'images', ListingImageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
